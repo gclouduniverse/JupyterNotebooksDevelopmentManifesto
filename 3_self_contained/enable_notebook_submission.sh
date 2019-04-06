deb026d6bf49c6bf4a9054427f4155e1c009b38f
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2019-04-06 14:25:13 -0700

    migration to the official gcp-notebook-executor 0.1.2

--- 3_self_contained/enable_notebook_submission.sh
+++ 3_self_contained/enable_notebook_submission.sh
@@ -17,9 +17,12 @@ if cat "${HOME}/.bashrc" | grep -q execute_notebook_with_cpu; then
     exit 0
 fi
 
-cat <<-'EOH' >> "${HOME}/.bashrc"
+readonly NOTEBOOK_UTILS_PATH="${HOME}/.notebook_utils.sh"
 
-readonly DEFAULT_NOTEBOOK_EXECUTOR_INSTANCE_NAME="notebookexecutor"
+wget https://raw.githubusercontent.com/gclouduniverse/gcp-notebook-executor/v0.1.2/utils.sh -O "${NOTEBOOK_UTILS_PATH}"
+
+cat <<-'EOH' >> "${HOME}/.bashrc"
+readonly NOTEBOOK_UTILS_PATH="${HOME}/.notebook_utils.sh"
 
 function execute_notebook_with_gpu() {
     if [ "$#" -ne 4 ]; then
@@ -31,66 +34,12 @@ function execute_notebook_with_gpu() {
         echo ""
         return 1
     fi 
-    IMAGE_FAMILY="tf-latest-cu100" # or put any required
-    ZONE="us-west1-b"
-    INSTANCE_NAME="${DEFAULT_NOTEBOOK_EXECUTOR_INSTANCE_NAME}"
-    INSTANCE_TYPE="n1-standard-8"
-    INPUT_NOTEBOOK=$1
-    TEMP_GCS_LOCATION=$2
-    INPUT_NOTEBOOK_GCS_PATH="${TEMP_GCS_LOCATION}/${INPUT_NOTEBOOK}"
+    INSTANCE_NAME=$1
+    GCP_PATH=$2
     GPU_TYPE=$3
     GPU_COUNT=$4
-    gsutil cp "./${INPUT_NOTEBOOK}" "${TEMP_GCS_LOCATION}"
-    if [[ $? -eq 1 ]]; then
-        echo "Upload to the temp GCS location (${TEMP_GCS_LOCATION}) of the notebook (./${INPUT_NOTEBOOK}) have failed."
-        return 1
-    fi
-    gcloud compute instances create "${INSTANCE_NAME}" \
-            --zone="${ZONE}" \
-            --image-family="${IMAGE_FAMILY}" \
-            --image-project=deeplearning-platform-release \
-            --maintenance-policy=TERMINATE \
-            --accelerator="type=nvidia-tesla-${GPU_TYPE},count=${GPU_COUNT}" \
-            --machine-type="${INSTANCE_TYPE}" \
-            --boot-disk-size=200GB \
-            --scopes=https://www.googleapis.com/auth/cloud-platform \
-            --metadata="input_notebook=${INPUT_NOTEBOOK_GCS_PATH},output_notebook=${TEMP_GCS_LOCATION},startup-script-url=https://raw.githubusercontent.com/b0noI/gcp-notebook-executor/master/notebook_executor.sh" \
-            --quiet
-    if [[ $? -eq 1 ]]; then
-        echo "Creation of background instance for trainin have failed."
-        return 1
-    fi
-    wait_till_instance_not_exist "${INSTANCE_NAME}" "${ZONE}"
-    echo "execution has been finished, downloading the result (notebook.ipynb)"
-    gsutil cp "${TEMP_GCS_LOCATION}/*" .
-    if [[ $? -eq 1 ]]; then
-        echo "There was a problem with downloading result notebook (${TEMP_GCS_LOCATION}/notebook.ipynb)."
-        return 1
-    fi
-    echo "done"
-    return 0
-}
-
-function wait_till_instance_not_exist() {
-    INSTANCE_NAME=$1
-    ZONE=$2
-    if [ "$#" -ne 2 ]; then
-        echo "Usage: "
-        echo "   ./wait_till_instance_not_exist [INSTANCE_NAME] [ZONE]"
-        echo ""
-        echo "example:"
-        echo "   ./wait_till_instance_not_exist instance1 us-west1-b"
-        echo ""
-        return 1
-    fi
-    while true; do
-        if gcloud compute instances describe "${INSTANCE_NAME}" --zone="${ZONE}" > /dev/null; then
-            echo "Background execution still in progress, going to sleep 10sec..."
-            sleep 10
-            continue
-        fi
-        return 0
-    done
+    execute_execute_notebook -i "${INSTANCE_NAME}" -o "${GCP_PATH}" -f tf-latest-gpu -g "${GPU_TYPE}" -c "${GPU_COUNT}"
+    return $?
 }
 
 function execute_notebook_with_cpu() {
@@ -103,40 +52,8 @@ function execute_notebook_with_cpu() {
         echo ""
         return 1
     fi 
-    IMAGE_FAMILY="tf-latest-cpu" # or put any required
-    ZONE="us-west1-b"
-    INSTANCE_NAME="${DEFAULT_NOTEBOOK_EXECUTOR_INSTANCE_NAME}"
-    INSTANCE_TYPE="n1-standard-8"
-    INPUT_NOTEBOOK=$1
-    TEMP_GCS_LOCATION=$2
-    INPUT_NOTEBOOK_GCS_PATH="${TEMP_GCS_LOCATION}/${INPUT_NOTEBOOK}"
-    gsutil cp "./${INPUT_NOTEBOOK}" "${TEMP_GCS_LOCATION}"
-    if [[ $? -eq 1 ]]; then
-        echo "Upload to the temp GCS location (${TEMP_GCS_LOCATION}) of the notebook (./${INPUT_NOTEBOOK}) have failed."
-        return 1
-    fi
-    gcloud compute instances create "${INSTANCE_NAME}" \
-            --zone="${ZONE}" \
-            --image-family="${IMAGE_FAMILY}" \
-            --image-project=deeplearning-platform-release \
-            --machine-type="${INSTANCE_TYPE}" \
-            --boot-disk-size=200GB \
-            --scopes=https://www.googleapis.com/auth/cloud-platform \
-            --metadata="input_notebook=${INPUT_NOTEBOOK_GCS_PATH},output_notebook=${TEMP_GCS_LOCATION},startup-script-url=https://raw.githubusercontent.com/b0noI/gcp-notebook-executor/master/notebook_executor.sh" \
-            --quiet
-    if [[ $? -eq 1 ]]; then
-        echo "Creation of background instance for trainin have failed."
-        return 1
-    fi
-    wait_till_instance_not_exist "${INSTANCE_NAME}" "${ZONE}"
-    echo "execution has been finished, downloading the result (notebook.ipynb)"
-    gsutil cp "${TEMP_GCS_LOCATION}/*" .
-    if [[ $? -eq 1 ]]; then
-        echo "There was a problem with downloading result notebook (${TEMP_GCS_LOCATION}/notebook.ipynb)."
-        return 1
-    fi
-    echo "done"
-    return 0
+    execute_execute_notebook -i "${INSTANCE_NAME}" -o "${GCP_PATH}" -f tf-latest-cpu
+    return $?
 }
 EOH
 
